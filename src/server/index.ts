69b65062810262c1cc154949d55e35b7c5909d8c
KidP2H <thjnhsoajca@gmail.com>
2022-04-02 07:47:20 +0700

    deploy to vps

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,23 +1,53 @@
 import http from 'http';
 import { Server } from 'socket.io';
-import InitializeServerSocket from './helper/SocketIO';
+import { SMS } from './helper/SocketIO/SMS';
+
+type PhoneNumber = {
+  phoneNumber: string;
+};
+function getReqData(req: http.IncomingMessage) {
+  return new Promise<string>((resolve, reject) => {
+    try {
+      let body = '';
+      // listen to data sent by client
+      req.on('data', (chunk) => {
+        // append the string version to the body
+        body += chunk.toString();
+      });
+      // listen till the end
+      req.on('end', () => {
+        // send back the data
+        resolve(body);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 const server = http.createServer(async (req, res) => {
   if (req.url == '/emitMessage' && req.method == 'POST') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write('...');
+    let body: string = await getReqData(req);
+    let result = JSON.parse(body);
+    SMS.Emit(io, result, 'emitMessage');
     res.end();
   }
   if (req.url == '/' && req.method == 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write('index');
+    res.write('...');
+    console.log('x');
     res.end();
   }
 });
 const io = new Server(server);
-InitializeServerSocket(io);
+io.on('connection', (socket) => {
+  socket.on('sendSuccess', (arg) => {
+    console.log(arg);
+  });
+});
 server.listen(5000, () => {
   console.log('Started on port: 5000');
 });
 
-export default server;
+export { server, io };
