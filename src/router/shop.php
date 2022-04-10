189c0287510e26bc7b478161fd560af508fddf08
daisgu014 <daibucky@gmail.com>
2022-04-10 13:51:54 +0700

    Updatecode 10/04/2022

--- src/router/shop.php
+++ src/router/shop.php
@@ -4,7 +4,10 @@ namespace router;
 use app\controllers\ContactController;
 use core\Application;
 use app\controllers\HomeController;
+use app\controllers\ProductController;
+use app\models\Product;
+
 $app = Application::Instance();
 $app->router->prefix("/shop");
-$app->router->get("/product", [ContactController::class, "shop"]);
+$app->router->get("/product", [ProductController::class,"shop"]);
 
