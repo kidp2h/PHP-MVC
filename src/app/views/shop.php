8812f03c402c97f3c54155db95704bdda5696eb0
daisgu014 <daibucky@gmail.com>
2022-04-13 15:24:45 +0700

    Dai upcode nè lêu lêu 13

--- src/app/views/shop.php
+++ src/app/views/shop.php
@@ -1,30 +1,4 @@
-<?php 
-                use \app\controllers\ProductController;
-?>                
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
-      integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= "/public/styles/shop/base.css" ?>">
-    <link rel="stylesheet" href="<?= "/public/styles/shop/apps.css" ?>">
-    <link rel="stylesheet" href="<?= "/public/styles/shop/rp.css" ?>">
-    <title>Document</title>
-</head>
-<body>
-<div id="shop">
+       <div id="shop">
         <div id="wrapper">
             <div id="product-intro">
                 <p>Products</p>
@@ -187,5 +161,3 @@
             </div>
         </div>
     </div>
-</body>
-</html>
