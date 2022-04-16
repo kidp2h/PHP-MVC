18d36fc860522d192aaa46f6c2e1cea7f5a34640
kidp2h <kidp2h@gmail.com>
2022-04-16 10:29:22 +0700

    update env

--- src/migrations.php
+++ src/migrations.php
@@ -1,16 +1,13 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
-$dotenv = Dotenv::createImmutable(__DIR__);
-
-$dotenv->load();
 use database\migrations\m001_initialize as mInitialize;
-class Migrations
-{
-  public function __construct()
-  {
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+class Migrations {
+  public function __construct() {
     new mInitialize();
   }
 }
-
 new Migrations();
