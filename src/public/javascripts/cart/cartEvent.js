7f589dbd4c25993d0fb5b805df65f4f4d3156bc0
LuongThinh123 <luongchithinh123@gmail.com>
2022-05-23 20:40:41 +0700

    chi thinh pull

--- src/public/javascripts/cart/cartEvent.js
+++ src/public/javascripts/cart/cartEvent.js
@@ -291,15 +291,6 @@ eventCart.init();
 //   return '';
 // }
 
-function getCookies() {
-  var cookies = document.cookie.split(';');
-  var ret = '';
-  for(var i = 1; i <= cookies.length; i++) {
-      ret += i + ' - ' + cookies[i - 1] + "<br>";
-  }
-  return ret;
-}
-
 function AddToCart() {
   let inputQuantity = $$('.inputQuantity');
   let addToCart = $$('.addToCart');
