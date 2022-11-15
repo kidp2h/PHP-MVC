372c9a0e4199e5a4496726cf7643b87a37a9150b
kidp2h <f7kingeagles@gmail.com>
2022-11-15 10:03:39 +0700

    testing

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,7 +1,10 @@
 FROM php:apache
-RUN apt-get update -y && apt-get upgrade -y
+RUN apt-get update -y && apt-get upgrade -y && apt-get install git -y
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 RUN a2enmod rewrite
-EXPOSE 80
+COPY ./src/composer.json .
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+ENV COMPOSER_ALLOW_SUPERUSER=1
+RUN composer install
 
 
