7b3e1ec612f79b3c14a10b70d3f5397aca630f1d
kidp2h <f7kingeagles@gmail.com>
2022-11-19 23:23:47 +0700

    test/signup

--- src/tests/AuthTest.php
+++ src/tests/AuthTest.php
@@ -16,26 +16,23 @@ class AuthTest extends TestCase
   private MockObject $responseMock;
   private MockObject $userMock;
 
-  public function testValidUser(): void
+
+  /**
+   * @dataProvider signInProvider
+   * @test
+   */
+  public function testSignIn(string $username, string $password, bool $status, string $expected): void
   {
     // Mocking
-    /** @var Request&MockObject $requestMock */
-    $this->requestMock = $this->createMock(Request::class);
-    /** @var Response&MockObject $responseMock */
-    $this->responseMock = $this->createMock(Response::class);
-    /** @var User&MockObject $userMock */
-    $this->userMock = $this->createMock(User::class);
-
-    // setup
     $this->requestMock->expects($this->any())
       ->method('method')
       ->will($this->returnValue("POST"));
 
     $this->requestMock->expects($this->any())
       ->method('body')
-      ->will($this->returnValue(["username" => "admin", "password" => "admin"]));
+      ->will($this->returnValue(["username" => $username, "password" => $password]));
 
-    $this->userMock->expects($this->any())->method("checkUser")->willReturn((object)["status" => true, "user" => (object)[
+    $this->userMock->expects($this->any())->method("checkUser")->willReturn((object)["status" => $status, "user" => (object)[
       "id" => 1
     ]]);
     $this->userMock->expects($this->any())->method("update")->willReturn(true);
@@ -43,13 +40,73 @@ class AuthTest extends TestCase
     // Inject Dependencies
     $controller = new ReflectionClass(AuthController::class);
     $controller->getProperty("userModel")->setValue($this->userMock);
-    $controller->getProperty("userModel")->getValue()->setPassword("admin");
+    $controller->getProperty("userModel")->getValue()->setPassword($password);
     $controller->getMethod("handleSignIn")->invoke(new AuthController(), $this->requestMock, $this->responseMock);
 
     //Assertion
     $this->assertEquals(
-      '{"status":true,"redirect":"\/"}',
+      $expected,
       $controller->getMethod("handleSignIn")->invoke(new AuthController(), $this->requestMock, $this->responseMock)
     );
+  }
+
+  public function signInProvider(): array
+  {
+    return [
+      "wrong" => ["admin1", "admin1", false, '{"status":false,"message":"Username or password is wrong"}'],
+      "correct" => ["admin", "admin", true, '{"status":true,"redirect":"\/"}']
+    ];
+  }
+
+
+  /**
+   * @dataProvider signUpProvider
+   * @test
+   */
+  public function testSignUp(string $username, string $password, string $email, bool $status, string $expected): void
+  {
+    // Mocking
+    $this->requestMock->expects($this->any())
+      ->method('method')
+      ->will($this->returnValue("POST"));
+
+    $this->requestMock->expects($this->any())
+      ->method('body')
+      ->will($this->returnValue(["username" => $username, "password" => $password, "email" => $email, "fullName" => "any"]));
+
+    $this->userMock->expects($this->any())->method("create")->willReturn((object)["status" => $status, "id" => 1]);
+    $this->userMock->expects($this->any())->method("update")->willReturn(true);
+
+    // Inject Dependencies
+    $controller = new ReflectionClass(AuthController::class);
+    $controller->getProperty("userModel")->setValue($this->userMock);
+    $controller->getMethod("handleSignUp")->invoke(new AuthController(), $this->requestMock, $this->responseMock);
+
+    //Assertion
+    $this->assertEquals(
+      $expected,
+      $controller->getMethod("handleSignUp")->invoke(new AuthController(), $this->requestMock, $this->responseMock)
+    );
+  }
+  public function signUpProvider(): array
+  {
+    return [
+      "not exist" => ["admin299", "admin299", "admin299@gmail.com", true, '{"status":true,"redirect":"\/signin"}'],
+      "exist username" => ["admin", "admin", "asmd@gmail.com", false, '{"status":false,"message":"Username or email is exist"}'],
+      "exist email" => ["adm23in", "adm23in", "kidp2h@gmail.com", false, '{"status":false,"message":"Username or email is exist"}']
+    ];
+  }
+  protected function setUp(): void
+  {
+    /** @var Request&MockObject $requestMock */
+    $this->requestMock = $this->createMock(Request::class);
+    /** @var Response&MockObject $responseMock */
+    $this->responseMock = $this->createMock(Response::class);
+    /** @var User&MockObject $userMock */
+    $this->userMock = $this->createMock(User::class);
+  }
+  protected function tearDown(): void
+  {
+    parent::tearDown();
   }
 }
